9acc69f49be3fb0c7f81ed8cd63e01f619f7e0db
Soumyadeep Dey <sd5046438@gmail.com>
2025-04-17 23:21:46 +0530

    all configers setupare completed and routes and controller added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,18 +1,23 @@
 class ApiError extends Error {
-  constructor(message="Something went wrong", statusCode,errors=[],stack="") {
+  constructor(
+    message = "Something went wrong",
+    statusCode,
+    errors = [],
+    stack = ""
+  ) {
     super(message);
     this.statusCode = statusCode;
     this.errors = errors;
     this.message = message;
     this.success = false;
-    this.data= null;
+    this.data = null;
 
     if (stack) {
-       this.stack = stack; 
-    }else{
-        Error.captureStackTrace(this, this.constructor);
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
 
-export {ApiError}
+export { ApiError };
