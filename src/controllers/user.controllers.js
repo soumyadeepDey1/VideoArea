764a662550399438038c224c27b709d217d976df
Soumyadeep Dey <sd5046438@gmail.com>
2025-04-25 13:30:34 +0530

    register controller fix bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { ApiResponse } from "../utils/ApiRespons.js"
+import { ApiResponse } from "../utils/ApiRespons.js";
 const registerUser = asyncHandler(async (req, res) => {
   //res.status(200).json({ message: 'chai aur code with Soumyadeep' });
   //get user details from frontend
@@ -15,8 +15,8 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for user creation
   //return res
 
-  const { fullName, email, username, password } = req.body;
-  console.log("email", email);
+  const { fullName, email, username, password } =  req.body;
+  //console.log("email", email);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -24,15 +24,19 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError("All field is required", 400);
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
     throw new ApiError("User already exists", 409);
   }
 
-  const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverLocalPath = req.files?.cover[0]?.path;
+  const avatarLocalPath =  req.files?.avatar[0]?.path;
+  //const coverLocalPath =  req.files?.cover[0]?.path;
+  let coverLocalPath;
+  if(req.files && Array.isArray(req.files.cover) && req.files.cover.length > 0) {
+    coverLocalPath = req.files.cover[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError("Avatar is required", 400);
@@ -44,28 +48,25 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError("Avatar is required", 400);
   }
 
-    const user = await User.create({
+  const user = await User.create({
     fullName,
-    avater: avatar.url,
+    avatar: avatar.url,
     cover: cover?.url || "",
     email,
     password,
-    username: username.toLowerCase()
-    
-  })
+    username: username.toLowerCase(),
+  });
 
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
-);
+  );
 
   if (!createdUser) {
     throw new ApiError("Something went wront when resistering the user", 500);
-    
   }
 
-  return res.status(201).json(
-    new ApiResponse("User created successfully",createdUser, 201)
-  )
-
+  return res
+    .status(201)
+    .json(new ApiResponse("User created successfully", createdUser, 201));
 });
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             lowercase:true,
             trim:true,
         },
-        fullname:{
+        fullName:{
             type: String,
             required:true,
             trim:true,
@@ -29,7 +29,7 @@ const userSchema = new Schema(
             type: String,
             required:true,
         },
-        coverImage:{
+        cover:{
             type: String,
         },
         watchHistory:[{
