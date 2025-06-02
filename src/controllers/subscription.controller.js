470c5d6298cc87bbb946a612153907ca8e385e77
Soumyadeep Dey <sd5046438@gmail.com>
2025-06-02 23:22:34 +0530

    feat: Implement tweet creation, retrieval, update, and deletion with error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -76,30 +76,35 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
   // controller to return channel list to which user has subscribed
-    if (!isValidObjectId(subscriberId)) {
-        throw new ApiError(400, "Invalid subscriber ID");
-    }
-    const subscriptions = await Subscription.find({ subscriber: subscriberId })
-        .populate("channel", "username email fullname avatar"); 
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid subscriber ID");
+  }
+  const subscriptions = await Subscription.find({
+    subscriber: subscriberId,
+  }).populate("channel", "username email fullname avatar");
 
-    if (!subscriptions || subscriptions.length === 0) {
-        return res
-            .status(200)
-            .json(new ApiResponse("No subscriptions found for this user", [], 200));
-    }
-
-    const subscribedChannels = subscriptions.map((sub) => ({
-        _id: sub.channel._id,
-        username: sub.channel.username,
-        fullname: sub.channel.fullname,
-        avatar: sub.channel.avatar,
-    }));
-
+  if (!subscriptions || subscriptions.length === 0) {
     return res
-        .status(200)
-        .json(
-            new ApiResponse("Subscribed channels retrieved successfully", subscribedChannels, 200)
-        );
+      .status(200)
+      .json(new ApiResponse("No subscriptions found for this user", [], 200));
+  }
+
+  const subscribedChannels = subscriptions.map((sub) => ({
+    _id: sub.channel._id,
+    username: sub.channel.username,
+    fullname: sub.channel.fullname,
+    avatar: sub.channel.avatar,
+  }));
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        "Subscribed channels retrieved successfully",
+        subscribedChannels,
+        200
+      )
+    );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,103 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  //TODO: create tweet
+  const { content } = req.body;
+  const userId = req.user._id;
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content is required");
+  }
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user ID");
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const tweet = new Tweet({
+    content,
+    owner: userId,
+  });
+  await tweet.save();
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "Tweet created successfully", tweet));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  // TODO: get user tweets
+  const { userId } = req.params;
+  if (!userId) {
+    throw new ApiError(400, "User ID is required");
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const tweets = await Tweet.find({ owner: userId })
+    .populate("owner", "username profilePicture")
+    .sort({ createdAt: -1 });
+  if (tweets.length === 0) {
+    return res
+      .status(404)
+      .json(new ApiResponse(404, "No tweets found for this user"));
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "User tweets retrieved successfully", tweets));
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  //TODO: update tweet
+  const { tweetId } = req.params;
+  const { content } = req.body;
+  if (!tweetId || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet ID");
+  }
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content is required");
+  }
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+  if (tweet.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to update this tweet");
+  }
+  tweet.content = content;
+  await tweet.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweet updated successfully", tweet));
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+  //TODO: delete tweet
+  const { tweetId } = req.params;
+  if (!tweetId || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet ID");
+  }
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+  if (tweet.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to delete this tweet");
+  }
+  await tweet.remove();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweet deleted successfully"));
+});
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
