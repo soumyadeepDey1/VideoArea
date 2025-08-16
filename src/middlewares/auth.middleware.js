f9fac9a4713ef5af94d11855bac20d56b915879c
Soumyadeep Dey <sd5046438@gmail.com>
2025-08-16 19:39:46 +0530

    fix: Simplify token retrieval and standardize error messages in JWT verification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,23 +6,23 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
-      (await req.cookies?.accessToken) ||
-      (await req.header("Authorization")?.replace("Bearer ", ""));
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
       throw new ApiError("Unauthorized request", 401);
     }
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // Ensure correct secret
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
     if (!user) {
-      throw new ApiError("Invalid Access token", 401); // Fixed typo
+      throw new ApiError("Invalid Access token", 401);
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(error?.message || "Invalid access Token", 401);
+    throw new ApiError(error?.message || "Invalid access token", 401);
   }
 });
